46aaaf4f2323ec25e87f88ed80435288a31d5b13
Travis Swicegood <development@domain51.com>
2013-02-24 10:08:41 -0600

    Remove all of the SeriesNode inline stuff (doesn't work yet)

--- armstrong/apps/series/admin.py
+++ armstrong/apps/series/admin.py
@@ -1,20 +1,11 @@
 from django.contrib import admin
-from django.contrib.contenttypes import generic
-
 
 from . import models
-
-
-class SeriesNodeInline(generic.GenericTabularInline):
-    model = models.SeriesNode
 
 
 class SeriesAdmin(admin.ModelAdmin):
     model = models.Series
 
-    inlines = [
-        SeriesNodeInline,
-    ]
     prepopulated_fields = {
         'slug': ('title', ),
     }
